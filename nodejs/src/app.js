5fd956fb689edaf0354252f2366be5ad79ac12c9
Moamen Hassan <MoamenAttia@outlook.com>
2019-04-13 23:16:16 +0200

    callback handled

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const { addUser, getUser, removeUser, getUsersInRoom } = require('./utils/users');
-const {generateMessage} = require('./utils//messages');
+const { generateMessage } = require('./utils//messages');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
@@ -17,9 +17,6 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     log('new websocket connection!');
     socket.on('join', (options) => {
-        const usernamex = options.username;
-        const roomx = options.room;
-        log(usernamex,roomx);
         const { error, user } = addUser({ id: socket.id, username: options.username, room: options.room });
         if (error) {
             return console.log(error);
@@ -33,18 +30,21 @@ io.on('connection', (socket) => {
         });
     });
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
-        // const filter = new Filter();
-        // if (filter.isProfane(message)) {
-        // return callback('Profanity is not allowed!');
-        // }
-        io.to(user.room).emit('message', generateMessage(user.username, message));
+        const filter = new Filter();
+        if (filter.isProfane(message)) {
+            return callback(false);
+        }
+        log(`A new Message From ${user.username} in the room ${user.room}, the message: ${message}`)
+        socket.broadcast.to(user.room).emit('message', generateMessage(user.username, message));
+        callback(true);
     });
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
         if (user) {
+            log(`${user.username} has left the room ${user.room}`);
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`));
             io.to(user.room).emit('roomData', {
                 room: user.room,
